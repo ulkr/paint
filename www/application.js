800b015931b9168c2a89aa4461357fb87e6fa829
ulkr <37193396+ulkr@users.noreply.github.com>
2018-06-14 21:37:56 +0200

    version 1.0.1

--- www/application.js
+++ www/application.js
@@ -3,5 +3,10 @@ document.addEventListener('deviceready', function() {
     console.log( 'DEVICE READY!' );
     $( document ).ready( function() {
       console.log( 'DOM READY!' );
+      var c = $('<canvas>', { width:$(window).width(),height:$(window).height() }).prependTo('body');
+
+      var ctx = c.get(0).getContext('2d');
+      ctx.fillRect(100,100,100,100);
+
     });
 });
